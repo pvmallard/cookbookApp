7bc0856ed2c9f0accaa584c314d85a6d5bd644b0
Mallory Prescott <mvprescott17@gmail.com>
2026-04-09 12:16:15 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='container'>
+      <h1>heya react</h1>
+    </div>
+  )
+}
+
+export default App
