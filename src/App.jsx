b79b3541d45a0da702ccca18e0e3abd82cbdd1c8
Mallory Prescott <mvprescott17@gmail.com>
2026-04-09 19:22:03 -0600

    starting pages
    - adding files to mimic sitemap and structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const APP_ID = "";
+  const APP_KEY = "";
+
+  const exampleReq = 
+    "";
+
   return (
     <div className='container'>
       <h1>heya react</h1>
